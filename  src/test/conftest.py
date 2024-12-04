a2b9825ccf7c985ad103f52f5c4870adaed9304d
Michael <mike@hematite.tech>
2024-12-04 10:20:24 -0800

    add core package configuratiojn

---
+++  src/test/conftest.py
@@ -0,0 +1,5 @@
+"""Pytest conftest file for test confiugration."""
+# NOTE: Do not add a __init__.py file to the test folder. It is counter
+# to pytest standards and will cause the test folder to be treated as a
+# package. This will can cause import errors and other issues.
+# https://docs.pytest.org/en/latest/explanation/goodpractices.html
